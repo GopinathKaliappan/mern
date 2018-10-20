b3572d2ade4e0c4cd61080739100006faac4f349
Mahdi afzal <mahdiafzal@gmail.com>
2018-10-20 16:07:32 +0330

    added config

---
+++ config/index.js
@@ -0,0 +1,7 @@
+// config/index.js
+const dbuser = 'vcart';
+const dbpassword = '@6112M2085';
+
+const MONGODB_URI = `mongodb://${dbuser}:${dbpassword}@ds137483.mlab.com:37483/mearn`;
+
+module.exports = MONGODB_URI;
--- server.js
+++ server.js
@@ -4,16 +4,19 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+const routes = require('./routes');
+
 // configure body parser for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // routes
-app.get('/', (req, res) => {
-	res.send('Hello from MERN');
-});
+// app.get('/', (req, res) => {
+//     res.send('Hello from MERN');
+// });
+app.use(routes);
 
 // Bootstrap server
 app.listen(PORT, () => {
-	console.log(`Server listening on port ${PORT}.`);
+    console.log(`Server listening on port ${PORT}.`);
 });
